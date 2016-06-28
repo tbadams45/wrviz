5eb8b5f4e3f81057a971ef951f581a9b9fb36e77
Tim Adams <tbadams45@gmail.com>
2016-06-28 13:01:38 -0400

    edit resilience and vulnerability to range from 0 to 1

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -160,13 +160,13 @@ get_bins <- function(metric){
     s1 <- seq(30, 80, 10)
     s2 <- seq(90, 130, 10)
   }
-  else if (metric == "resilience") {
-    s1 <- c(seq(10, 60, 10))
-    s2 <- c(seq(seq(70,100,5)))
+  else if (metric == "resilience") { # values range from 0 to 1
+    s1 <- c(seq(0.10, 0.60, 0.10))
+    s2 <- c(seq(seq(0.70, 1, 0.05)))
   }
-  else if (metric == "vulnerability") {
-    s1 <- c(seq(10, 60, 10))
-    s2 <- c(seq(seq(70,100,5)))
+  else if (metric == "vulnerability") { # values range from 0 to 1
+    s1 <- c(seq(0.10, 0.60, 0.10))
+    s2 <- c(seq(seq(0.70,1,0.05)))
   } else {
     stop("Please enter a valid metric.")
   }
