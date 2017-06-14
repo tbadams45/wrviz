35da48b9f6998bec138b3a042a00744a34a0fb81
Timothy Adams <tbadams45@gmail.com>
2017-06-14 17:02:36 -0400

    enable editing of gcms

--- R/climate_response_addin.R
+++ R/climate_response_addin.R
@@ -7,6 +7,7 @@
 #'   (x$metrics and x$gcm)
 #' @export
 climateResponseCreator <- function() {
+  ALL_SCENARIOS <- c("RCP 2.6", "RCP 4.5", "RCP 6.0", "RCP 8.5")
 
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Climate Response Creator"),
@@ -47,15 +48,14 @@ climateResponseCreator <- function() {
         ) # close miniContentPanel
       ), # close miniTabPanel
 
-      miniUI::miniTabPanel("Edit", icon = shiny::icon("pencil"),
+      miniUI::miniTabPanel("Edit Chart", icon = shiny::icon("area-chart"),
         miniUI::miniContentPanel(
-          shiny::fillRow(flex = c(3,2),
+          shiny::fillRow(flex = c(3, 2),
 
             shiny::fillRow( # outputs
               shiny::fillCol(width = "95%", shiny::tagList(
                 shiny::uiOutput("output_column_controls"),
                 shiny::uiOutput("eval_type_option"),
-                shiny::uiOutput("gcm_option"),
                 shiny::uiOutput("ascending_option"),
                 shiny::uiOutput("format_as_percentage_controls")
               )),
@@ -73,13 +73,30 @@ climateResponseCreator <- function() {
             )
           )
         ) # close miniContentPanel
-      ) # close miniTabPanel
+      ), # close miniTabPanel
+
+      miniUI::miniTabPanel("Add GCMS", icon = shiny::icon("table"),
+        miniUI::miniContentPanel(
+          shiny::fillRow(flex = c(3, 2),
+            shiny::fillCol(shiny::tagList(
+              shiny::uiOutput("gcm_option"),
+              shiny::uiOutput("scenario_selection_option"),
+              shiny::actionButton("deselect_all", "Deselect all models"),
+              shiny::actionButton("select_all", "Select all models"),
+              DT::dataTableOutput("selected_models")
+            )),
+            shiny::fillRow(
+              shiny::plotOutput("plot2", height = "400px")
+            )
+          )
+        )
+      )
     ) # close miniTabstripPanel
   ) # close miniPage
 
   server <- function(input, output, session) {
     demo_data <- shiny::reactive({
-      if(input$demo_data == 0) {
+      if (input$demo_data == 0) {
         return(NULL)
       }
 
@@ -89,7 +106,7 @@ climateResponseCreator <- function() {
 
 
     data <- shiny::reactive({
-      if(!is.null(demo_data())) {
+      if (!is.null(demo_data())) {
         return(demo_data())
       }
 
@@ -105,7 +122,7 @@ climateResponseCreator <- function() {
     })
 
     demo_gcm_data <- shiny::reactive({
-      if(input$demo_data == 0) {
+      if (input$demo_data == 0) {
         return(NULL)
       }
       file_name <- system.file("extdata/raw_cmip5.xlsx", package = "wrviz")
@@ -114,25 +131,34 @@ climateResponseCreator <- function() {
     })
 
 
+    # raw GCM data from file/demo data
     gcm_data <- shiny::reactive({
-      if(!is.null(demo_gcm_data())) {
-        return(demo_gcm_data())
+      if (!is.null(demo_gcm_data())) {
+        result <- demo_gcm_data()
+      } else {
+        in_file <- input$gcm_file
+
+        if (is.null(in_file)) {
+          return(NULL)
+        }
+
+        extension <- strsplit(in_file$name, "[.]")[[1]][2] # split on dot.
+        file.rename(in_file$datapath, paste(in_file$datapath, ".", extension, sep = ""))
+        result <- parse_gcm(paste(in_file$datapath, ".", extension, sep = ""))
       }
 
-      in_file <- input$gcm_file
-
-      if (is.null(in_file)) {
-        return(NULL)
-      }
-
-      extension <- strsplit(in_file$name, "[.]")[[1]][2] # split on dot.
-      file.rename(in_file$datapath, paste(in_file$datapath, ".", extension, sep = ""))
-      parsed_data <- parse_gcm(paste(in_file$datapath, ".", extension, sep = ""))
-
-      parsed_data
+      result
     })
 
-    plot <- shiny::reactive({
+    # selected models from table, used to generate plot
+    gmc_data_selected_models <- shiny::reactive({
+      result <- gcm_data()
+      result <- result[input$selected_models_rows_selected, ]
+      result <- dplyr::filter(result, !is.na(temp)) # get rid of rows that are NA
+    })
+
+    # basic plot does not include GCMs.
+    basic_plot <- shiny::reactive({
       if (is.null(input$output_columns) ||
           is.null(input$is_continuous_scale) ||
           is.null(input$range_min) ||
@@ -185,17 +211,6 @@ climateResponseCreator <- function() {
         )
       }
 
-      if (input$gcm) {
-        temp_plot <- temp_plot +
-          ggplot2::geom_point(
-            data = gcm_data(),
-            ggplot2::aes(x = temp, y = precip, shape = scenario),
-            size = 2,
-            stroke = 1.5) +
-          ggplot2::scale_shape_manual(name = "Scenarios",
-                                      values = c(21, 22, 23, 24))
-      }
-
       if (input$x_axis_units == "%") {
         x_lab <- paste(input$x_axis_title, " (%)")
       } else {
@@ -212,8 +227,47 @@ climateResponseCreator <- function() {
       temp_plot
     })
 
+    gcm_plot <- shiny::reactive({
+      if (is.null(gcm_data()) ||
+          is.null(input$show_gcm)) {
+        return(NULL)
+      }
+
+      if (input$show_gcm == FALSE) {
+        temp_plot <- basic_plot()
+      } else {
+        data <- gmc_data_selected_models()
+
+        temp_plot <- basic_plot() + ggplot2::geom_point(
+          data = data,
+          ggplot2::aes(x = temp, y = precip, shape = scenario),
+          size = 2,
+          stroke = 1.5) +
+          ggplot2::scale_shape_manual(name = "Scenarios",
+            values = c(21, 22, 23, 24))
+      }
+
+      temp_plot
+    })
+
     output$plot <- shiny::renderPlot({
-      plot()
+      if (is.null(input$show_gcm) ||
+          input$show_gcm == FALSE) {
+        temp_plot <- basic_plot()
+      } else {
+        temp_plot <- gcm_plot()
+      }
+      temp_plot
+    })
+
+    output$plot2 <- shiny::renderPlot({
+      if (is.null(input$show_gcm) ||
+          input$show_gcm == FALSE) {
+        temp_plot <- basic_plot()
+      } else {
+        temp_plot <- gcm_plot()
+      }
+      temp_plot
     })
 
     output$upload_complete_notification <- shiny::renderUI({
@@ -229,7 +283,7 @@ climateResponseCreator <- function() {
 
     output$output_column_controls <- shiny::renderUI({
       if (is.null(data())) {
-        return(NULL)
+        return(shiny::p("Please upload your metric data on the upload tab."))
       }
       col_names <- colnames(data())
       col_names <- col_names[!col_names %in% c("temp", "precip")] # remove temp and precip columns, so we only have output columns
@@ -245,18 +299,6 @@ climateResponseCreator <- function() {
         c("Continuous" = TRUE, "Binary" = FALSE),
         selected = TRUE,
         inline = TRUE)
-    })
-
-    output$gcm_option <- shiny::renderUI({
-      if (is.null(data())) {
-        return(NULL)
-      }
-
-      if(is.null(gcm_data())) {
-        shiny::checkboxInput("gcm", "Show GCMs", value = FALSE)
-      } else {
-        shiny::checkboxInput("gcm", "Show GCMs", value = TRUE)
-      }
     })
 
     output$ascending_option <- shiny::renderUI({
@@ -339,8 +381,77 @@ climateResponseCreator <- function() {
       )
     })
 
+    output$gcm_option <- shiny::renderUI({
+      if (is.null(data())) {
+        return(shiny::p("Please upload your metric and GCM data on the upload tab."))
+      }
+
+      if (is.null(gcm_data())) {
+        view <- shiny::checkboxInput("show_gcm", "Show GCMs", value = FALSE)
+      } else {
+        view <- shiny::checkboxInput("show_gcm", "Show GCMs", value = TRUE)
+      }
+
+      view
+    })
+
+    output$scenario_selection_option <- shiny::renderUI({
+      gcms <- gcm_data()
+      scenarios <- ALL_SCENARIOS
+      shiny::checkboxGroupInput("selected_scenarios",
+                                "Filter by scenarios",
+                                choices = scenarios,
+                                selected = scenarios,
+                                inline = TRUE)
+    })
+
+
+    output$selected_models <- DT::renderDataTable(
+      DT::datatable(gcm_data(),
+                      selection = list(mode = "multiple",
+                                       selected = 1:nrow(gcm_data())),
+                      extensions = "Scroller",
+                      options = list(
+                        deferRender = TRUE,
+                        scrollY = 400,
+                        scroller = TRUE)) %>%
+          DT::formatRound("temp", 3) %>%
+          DT::formatRound("precip", 3)
+      )
+
+
+    dt_proxy <- DT::dataTableProxy("selected_models")
+    shiny::observeEvent(input$deselect_all, {
+      dt_proxy %>% DT::selectRows(numeric(0))
+      shiny::updateCheckboxGroupInput(session, "selected_scenarios", selected = character(0))
+    })
+
+    shiny::observeEvent(input$select_all, {
+      dt_proxy %>% DT::selectRows(1:nrow(gcm_data()))
+      shiny::updateCheckboxGroupInput(session, "selected_scenarios", selected = ALL_SCENARIOS)
+    })
+
+    shiny::observeEvent(input$selected_scenarios, {
+      currently_selected <- tibble::rowid_to_column(gcm_data())
+
+      print(currently_selected)
+
+      print(input$selected_scenarios)
+      print(currently_selected$scenario %in% input$selected_scenarios)
+      rows_to_keep <- currently_selected[currently_selected$scenario %in% input$selected_scenarios,]
+
+      dt_proxy %>% DT::selectRows(rows_to_keep$rowid)
+    })
+
     shiny::observeEvent(input$done, {
-      val <- list(metrics = data(), gcm = gcm_data(), plot = plot())
+      if (is.null(input$show_gcm) ||
+          input$show_gcm == FALSE) {
+        plot <- basic_plot()
+      } else {
+        plot <- gcm_plot()
+      }
+
+      val <- list(metrics = data(), gcm = gmc_data_selected_models(), plot = plot)
       print("Note: if you forgot to save your results in a variable, they're stored in .Last.value")
 
       shiny::stopApp(val)
@@ -350,6 +461,6 @@ climateResponseCreator <- function() {
   shiny::runGadget(ui,
                    server,
                    viewer = shiny::dialogViewer("Climate Reponse Explorer",
-                                                 width = 1200,
-                                                 height = 750))
+                                                width = 1200,
+                                                height = 750))
 }
